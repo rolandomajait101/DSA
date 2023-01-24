a6cf8371bf50fbd0b31b43b41e266d54fedad610
Rolando Majait <114267565+rolandomajait101@users.noreply.github.com>
2023-01-24 23:07:05 +0800

    Add files via upload

---
+++ Majait_Rolando_act2.py
@@ -0,0 +1,19 @@
+def soluton(list_1,list_2):
+  return
+
+list_1 =[1,2,3,4,5]
+list_2 =[2,4,5,6,7]
+list_3 =[]
+
+for i in range (len(list_1)):
+    list_3.append(list_1[i] - list_2[i])
+
+
+print(soluton)
+
+def solution(list_1,list_2):
+    return
+
+list_1 =[]
+list_2 =[]
+list_3 =[]
